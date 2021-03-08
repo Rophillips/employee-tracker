d4e86b9d1d32c7bb91fcdcd2ca6eb5f2ba30b1f4
Roseanne Phillips <rphillips.rp@gmail.com>
2021-03-07 20:30:17 -0500

    creating tables schema file

--- employeeSchema.sql
+++ employeeSchema.sql
@@ -4,4 +4,17 @@ CREATE DATABASE employee_Tracker_DB;
 
 USE employee_Tracker_DB;
 
+--create department table 
+CREATE TABLE department (
+    id int NOT NULL AUTO_INCREMENT,
+    name varchar(30) NOT NULL,
+    PRIMARY KEY (id)
+);
 
+CREATE TABLE role (
+    id int NOT NULL AUTO_INCREMENT,
+    title varchar(30) NOT NULL,
+    salary DECIMAL(8,2) NOT NULL, 
+    department_id int NOT NULL, 
+    PRIMARY KEY (department_id)
+);
