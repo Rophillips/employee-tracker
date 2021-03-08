36a306c4d37f6c7c879303c9ed2c7f7b53878ea0
Roseanne Phillips <rphillips.rp@gmail.com>
2021-03-08 16:15:35 -0500

    added 3 tables to schema file

--- employeeSchema.sql
+++ employeeSchema.sql
@@ -6,15 +6,31 @@ USE employee_Tracker_DB;
 
 --create department table 
 CREATE TABLE department (
+    -- unique number to be generated
     id int NOT NULL AUTO_INCREMENT,
     name varchar(30) NOT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
-    id int NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT,
     title varchar(30) NOT NULL,
     salary DECIMAL(8,2) NOT NULL, 
     department_id int NOT NULL, 
-    PRIMARY KEY (department_id)
+    PRIMARY KEY (id),
+
+    -- connecting tables with FOREIGN KEY clause and referencing with REFERENCE clause
+    FOREIGN KEY (department_id) REFERENCES department(id),
+
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name varchar(30),
+    last_name varchar(30),
+    role_id INT,
+    manager_id INT,
+    -- PRIMARY KEY (id),
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
