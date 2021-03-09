95e69910268cf4066d9996feee5900a630fb03be
Roseanne Phillips <rphillips.rp@gmail.com>
2021-03-08 22:13:46 -0500

    created department and role seeds

--- employeeSeeds.sql
+++ employeeSeeds.sql
@@ -1,1 +1,38 @@
 USE employee_Tracker_DB;
+
+-- Department seeds
+INSERT INTO department (name)
+VALUE ("Sales");
+
+INSERT INTO department (name)
+VALUE ("Engineering");
+
+INSERT INTO department (name)
+VALUE ("Finance");
+
+INSERT INTO department (name)
+VALUE ("Human Resources");
+
+
+-- Employee role seeds
+INSERT INTO role (title, salary, department_id);
+VALUE ("Sales Lead", 95000, 1);
+
+INSERT INTO role (title, salary, department_id);
+VALUE ("Sales Executive", 80000, 1);
+
+INSERT INTO role (title, salary, department_id);
+VALUE ("Lead Engineer", 150000, 2);
+
+INSERT INTO role (title, salary, department_id);
+VALUE ("Software Engineer", 100000, 2);
+
+INSERT INTO role (title, salary, department_id);
+VALUE ("Accountant", 120000, 3);
+
+INSERT INTO role (title, salary, department_id);
+VALUE ("Human Resources Manager", 125000, 4);
+
+INSERT INTO role (title, salary, department_id);
+VALUE ("Human Resources Associate", 95000, 1);
+
