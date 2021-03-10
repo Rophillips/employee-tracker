ca69189f221b668f9e1a9835b017faef02430d34
Roseanne Phillips <rphillips.rp@gmail.com>
2021-03-10 11:01:43 -0500

    connection successful

--- employeeSeeds.sql
+++ employeeSeeds.sql
@@ -15,24 +15,46 @@ VALUE ("Human Resources");
 
 
 -- Employee role seeds
-INSERT INTO role (title, salary, department_id);
+INSERT INTO role (title, salary, department_id)
 VALUE ("Sales Lead", 95000, 1);
 
-INSERT INTO role (title, salary, department_id);
+INSERT INTO role (title, salary, department_id)
 VALUE ("Sales Executive", 80000, 1);
 
-INSERT INTO role (title, salary, department_id);
+INSERT INTO role (title, salary, department_id)
 VALUE ("Lead Engineer", 150000, 2);
 
-INSERT INTO role (title, salary, department_id);
+INSERT INTO role (title, salary, department_id)
 VALUE ("Software Engineer", 100000, 2);
 
-INSERT INTO role (title, salary, department_id);
+INSERT INTO role (title, salary, department_id)
 VALUE ("Accountant", 120000, 3);
 
-INSERT INTO role (title, salary, department_id);
+INSERT INTO role (title, salary, department_id)
 VALUE ("Human Resources Manager", 125000, 4);
 
-INSERT INTO role (title, salary, department_id);
+INSERT INTO role (title, salary, department_id)
 VALUE ("Human Resources Associate", 95000, 1);
+
+-- Employee seeds
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUE ("Samantha", "Castricone", 2, null);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUE ("Daniel", "Castricone", 4, 1);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUE ("John", "Castricone", 1, null);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUE ("Tina", "King", 1, null);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUE ("Morgan", "Halloran", 4, null);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUE ("Maritza", "Cruz", 3, null);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUE ("Tricia", "McMahon", 2, null);
 
--- employeeTracker.js
+++ employeeTracker.js
@@ -7,6 +7,15 @@ const cTable = require('console.table');
 const connection = mysql.createConnection ({
     host: "localhost",
     user: "root",
-    password: "",
+    password: "Eddieluna25!",
+    port: 3306,
     database: "employee_Tracker_DB"
 });
+
+//Connection
+connection.connect(function(err){
+    if (err) throw err;
+    console.log("Connection Id " + connection.threadID)
+    //startQuestions();
+    
+})
